fee8f69e493e29c5eda157a0f317a5afc3f278c5
Salahdin Lahnoud <salahdin.lahnoud@gmail.com>
2017-02-01 09:56:12 +0100

    Test StokController

---
+++ WebShopProject/src/main/java/com/WebShopProject/controller/StokController.java
@@ -0,0 +1,22 @@
+package com.WebShopProject.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.WebShopProject.dao.StockRepository;
+
+@Controller
+public class StokController {
+
+	
+	
+	@Autowired
+	private StockRepository stokRepo;
+	
+	
+	
+	
+	
+	
+	
+}
